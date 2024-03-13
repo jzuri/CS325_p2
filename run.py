f7244a649be92481bdf342f5b2de84a23719c534
jzuri <123331421+jzuri@users.noreply.github.com>
2024-03-12 22:30:07 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
+from Modules import file_handling, data_processing
 
+def main():
+    # Reads URLs from the text file
+    urls = file_handling.read_urls_from_file('Data/raw/article_urls.txt')
+
+    # Scrapes content/text for each URL
+    for url in urls:
+        article_content = data_processing.scrape_article(url)
+        if article_content:
+            # Save the content to a text file
+            file_handling.save_content_to_file('Data/processed/articles.txt', url, article_content)
+
+if __name__ == "__main__":
+    main()
