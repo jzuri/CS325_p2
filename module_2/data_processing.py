42ab68a405e6ddfc1c4e5a3330d8255dfc97aa5c
jzuri <123331421+jzuri@users.noreply.github.com>
2024-03-12 22:31:31 -0500

    Update data_processing.py

--- module_2/data_processing.py
+++ module_2/data_processing.py
@@ -1,1 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_article(url):
+    """Scrape article content from a given URL."""
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        article_content = soup.find('div', class_='showBlogPage')
+        if article_content:
+            return article_content.get_text(separator='\n')
+        else:
+            return "Just go ahead and change your major =P"
+    except requests.exceptions.RequestException as e:
+        print("Error fetching URL.")
+        return None
 
